4ca27cad9053decd64a83f0f92d00d33ec23da9e
faumar camara <faumar_57@hotmail.com>
2025-05-17 10:24:48 -0300

    .
    .

--- api/index.js
+++ api/index.js
@@ -13,10 +13,10 @@ app.use('/api/users', require('./routes/users'));
 
 // Serve o front-end em produção
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
   });
 }
 
